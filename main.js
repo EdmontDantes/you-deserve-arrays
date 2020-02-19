72cf52bc2da5034a581829114dd401ec0e831cc8
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-19 15:49:06 -0500

    14 passed

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom (array) {
+  return array[0];
+}
 
+function getLastItemFrom (array) {
+  return array.pop();
+}
 
+function getIndex3 (array) {
+  if (array.length >= 4) {
+  return array[3];
+  } else if (array.length < 4) {
+    return array.pop();
+  }
+}
 
+function isLongList (array) {
+  if (array.length >= 10) {
+    return true;
+  } else if (array.length < 10) {
+    return false;
+  }
+}
 
+function firstItemIsNumber (array) {
+  if (typeof array[0] == 'number') {
+    return true;
+  } else if (isNaN(array[0])) {
+    return false;
+  } else if (typeof array[0] == 'string') {
+    return false;
+  } else if (typeof array[0] == 'boolean') {
+    return false;
+  } else if (typeof array[0] == 'function') {
+    return false;
+  } else if (typeof array[0] == 'object') {
+    return false;
+  }
+}
 
+function secondCharOfThirdString (array) {
+  return array[2][1];
+}
 
 
  /********************************************
